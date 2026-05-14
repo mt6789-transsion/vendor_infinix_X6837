c0212cf25c91c57cffd4286e0866713603360588
vlxlxlv <fajarslebew31@gmail.com>
2026-05-14 15:35:09 +0200

    X6837: Sync with dt changes
    Change-Id: I053041647237a5830f63b8aac865b634d1a850af
    Signed-off-by: vlxlxlv <fajarslebew31@gmail.com>

--- Android.mk
+++ Android.mk
@@ -3,3 +3,20 @@
 #
 
 LOCAL_PATH := $(call my-dir)
+
+ifeq ($(TARGET_DEVICE),X6837)
+
+$(call add-radio-file-sha1-checked,radio/dpm.img,3d96e838ae076450ee6dc26d4bf3d8b88dc22bb4)
+$(call add-radio-file-sha1-checked,radio/gz.img,50bcb4cf4466ce9a501d13c8bdede2620b995b0f)
+$(call add-radio-file-sha1-checked,radio/lk.img,d93f0a00e8ae064195141c154cdbb89f6f7bbd63)
+$(call add-radio-file-sha1-checked,radio/logo.img,a448ca280ecff44130515fbe93fb38cce0fbd1bb)
+$(call add-radio-file-sha1-checked,radio/mcupm.img,be062ba5b6952c11e73093dd7b6cd55062892cb8)
+$(call add-radio-file-sha1-checked,radio/md1img.img,0c595b8999e38d69f6e1ec8a93a8c569ea0dba39)
+$(call add-radio-file-sha1-checked,radio/pi_img.img,d49bfeade6d8b7d709b22215528d2cbdf93a729b)
+$(call add-radio-file-sha1-checked,radio/scp.img,503a0b4924780a65a44a62b622ebb71405b1aa73)
+$(call add-radio-file-sha1-checked,radio/spmfw.img,df19212edd4c9f96391dda247601a92df0a37db6)
+$(call add-radio-file-sha1-checked,radio/sspm.img,66cf40d3d65ef7b23b29f460e945a65d720ce5fa)
+$(call add-radio-file-sha1-checked,radio/tee.img,30505a4540fa5002766034563e8f11a84ffd7771)
+$(call add-radio-file-sha1-checked,radio/tkv.img,2f3600a53b076e51e023e180f358b198e285fceb)
+
+endif
